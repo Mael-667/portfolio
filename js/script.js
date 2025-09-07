6e163eb111067afec3b2aa2c63153ac201b8865f
Mael-667 <flamentmael@gmail.com>
2025-09-07 19:20:51 +0200

    eclairage dynamique

--- js/script.js
+++ js/script.js
@@ -23,6 +23,134 @@ function toggleAnim(target, cssClass){
     target.classList.toggle(cssClass);
 }
 
+function triFusion(array, fun = (leftEntry, rightEntry) => leftEntry > rightEntry ){
+
+    function defusion(arrayCopy){
+        if(arrayCopy.length == 1){
+            return arrayCopy;
+        } else {
+            let g, d;
+            g = arrayCopy.slice(0, arrayCopy.length/2);
+            d = arrayCopy.slice(arrayCopy.length/2, arrayCopy.length);          
+            return fusion(defusion(g), defusion(d))
+        }
+    }
+
+    function fusion(arrG, arrD){
+        let ans = new Array(arrG.length + arrD.length);
+        let ig, id;
+        ig = id = 0;
+        for(let i = 0; i < ans.length; ++i){
+            if(ig == arrG.length){
+                ans[i] = arrD[id++];
+            } else if(id == arrD.length){
+                ans[i] = arrG[ig++];
+            } else if (fun(arrG[ig], arrD[id])){
+                ans[i] = arrG[ig++];
+            } else {
+                ans[i] = arrD[id++];
+            }
+        }
+        return ans;
+    }
+
+    return defusion(array);
+}
+
+function isTargetInElement(target, element){
+    let coTarget = target.getBoundingClientRect();
+    let coElement = element.getBoundingClientRect();
+    if(!(coTarget.top >= coElement.top)){
+        return false;
+    }
+    if(!(coTarget.bottom <= coElement.bottom)){
+        return false;
+    }
+    if(!(coTarget.left >= coElement.left)){
+        return false;
+    }
+    if(!(coTarget.right <= coElement.right)){
+        return false;
+    }
+    return true;
+}
+
+
+
+//ajouter "data-hue:#hexcolor" a un élement pour que sa couleur imprègne les liquidglass ayant pour parent un élément a la class dynamic qui le survolent
+function dynamicColor(){
+
+    let backgrounds = document.querySelectorAll("[data-hue]")
+    let parents = document.querySelectorAll(".dynamic")
+    let elmnts = [];
+    for(parent of parents){ 
+        for(enfant of parent.querySelectorAll(".liquidGlass")){
+            elmnts.push(enfant);
+        }
+    } 
+
+
+    addEvt(document, "scroll", function(){
+        let selected = [];
+
+        for(let i = 0; i < backgrounds.length; ++i){
+            let color = backgrounds[i].getAttribute("data-hue");
+            for(let j = 0; j < elmnts.length; j++){
+                if(isTargetInElement(elmnts[j], backgrounds[i])){
+                    elmnts[j].classList.add("dynamic")
+                    elmnts[j].style.setProperty('--background', `${pSBC(0.01, color)}07`);
+                    elmnts[j].style.setProperty('--shadow', `inset 1px 1px 3px 1px ${pSBC(-0.2, color)}20, inset -1px -2px 3px 1px rgb(49 49 49 / 30%), 0px 0px 7px 1px rgb(0 0 0 / 46%)`);
+                    selected.push(j);
+                }
+            }
+        }
+        for(let i = 0; i < elmnts.length; ++i){
+            let euh = true;
+            for(let j = 0; j < selected.length; j++){
+                if(i == selected[j]){
+                    euh = false;
+                }
+            }
+            if(euh){
+                elmnts[i].classList.remove("dynamic")
+                elmnts[i].style.setProperty('--background', "");
+                elmnts[i].style.setProperty('--shadow', ``);
+            }
+        }
+    })
+}
+
+
+
+const pSBC=(p,c0,c1,l)=>{
+    let r,g,b,P,f,t,h,i=parseInt,m=Math.round,a=typeof(c1)=="string";
+    if(typeof(p)!="number"||p<-1||p>1||typeof(c0)!="string"||(c0[0]!='r'&&c0[0]!='#')||(c1&&!a))return null;
+    if(!this.pSBCr)this.pSBCr=(d)=>{
+        let n=d.length,x={};
+        if(n>9){
+            [r,g,b,a]=d=d.split(","),n=d.length;
+            if(n<3||n>4)return null;
+            x.r=i(r[3]=="a"?r.slice(5):r.slice(4)),x.g=i(g),x.b=i(b),x.a=a?parseFloat(a):-1
+        }else{
+            if(n==8||n==6||n<4)return null;
+            if(n<6)d="#"+d[1]+d[1]+d[2]+d[2]+d[3]+d[3]+(n>4?d[4]+d[4]:"");
+            d=i(d.slice(1),16);
+            if(n==9||n==5)x.r=d>>24&255,x.g=d>>16&255,x.b=d>>8&255,x.a=m((d&255)/0.255)/1000;
+            else x.r=d>>16,x.g=d>>8&255,x.b=d&255,x.a=-1
+        }return x};
+    h=c0.length>9,h=a?c1.length>9?true:c1=="c"?!h:false:h,f=this.pSBCr(c0),P=p<0,t=c1&&c1!="c"?this.pSBCr(c1):P?{r:0,g:0,b:0,a:-1}:{r:255,g:255,b:255,a:-1},p=P?p*-1:p,P=1-p;
+    if(!f||!t)return null;
+    if(l)r=m(P*f.r+p*t.r),g=m(P*f.g+p*t.g),b=m(P*f.b+p*t.b);
+    else r=m((P*f.r**2+p*t.r**2)**0.5),g=m((P*f.g**2+p*t.g**2)**0.5),b=m((P*f.b**2+p*t.b**2)**0.5);
+    a=f.a,t=t.a,f=a>=0||t>=0,a=f?a<0?t:t<0?a:a*P+t*p:0;
+    if(h)return"rgb"+(f?"a(":"(")+r+","+g+","+b+(f?","+m(a*1000)/1000:"")+")";
+    else return"#"+(4294967296+r*16777216+g*65536+b*256+(f?m(a*255):0)).toString(16).slice(1,f?undefined:-2)
+}
+
+
+
+
+
 
 
 let intro = document.querySelector("#intro");
@@ -32,7 +160,7 @@ document.addEventListener("scroll", function(e){
     let euh = isElementInViewport(intro); 
     if(euh){
         document.querySelectorAll("header .liquidGlass").forEach(e => 
-            e.classList.contains("glassDarkMode") ? e.classList.remove("glassDarkMode") : 0
+            e.classList.remove("glassDarkMode")
         );
     } else {
         document.querySelectorAll("header .liquidGlass").forEach(e =>
@@ -199,7 +327,8 @@ async function getGithubData() {
       throw new Error(`Response status: ${response.status}`);
     }
 
-    const result = await response.json();
+    let result = await response.json();
+    result = triFusion(result, (g, d) => g.id < d.id)
     localStorage.setItem("github", result);
     console.log(result);
 
@@ -428,7 +557,7 @@ function updateCursor(e){
     
     trueOffset = Math.abs(trueOffset-100);
     trueOffset = (trueOffset*scrollLeft/100);
-    let barposition;
+    let barposition = cursorRelativePos-(cursor.offsetWidth/2);
     let gradientPos = cursorRelativePos;
     if(cursorRelativePos-(cursor.offsetWidth/2) < 0){
         barposition = 0;
@@ -436,8 +565,6 @@ function updateCursor(e){
     } else if (cursorRelativePos-(cursor.offsetWidth/2) > spaceTotal){
         barposition = spaceTotal;
         gradientPos = scrollBar.offsetWidth - cursor.offsetWidth/2 + 5;
-    } else {
-        barposition = cursorRelativePos-(cursor.offsetWidth/2);
     }
     get("#elements").style = `background-image : radial-gradient(circle at ${gradientPos}px,rgba(255, 221, 235, 1) ${(cursor.offsetWidth/2)-5}px, rgba(31, 0, 23, 1) ${(cursor.offsetWidth/2)}px) !important;`
     get("#line").style = `background-image : radial-gradient(circle at ${gradientPos}px,rgba(255, 221, 235, 1) ${(cursor.offsetWidth/2)-5}px, rgba(31, 0, 23, 1) ${(cursor.offsetWidth/2)}px) !important;`
@@ -531,4 +658,30 @@ function scrollAnimTo0(){
         carrousel.scrollTo(target, 0);
         animId2 = requestAnimationFrame(scrollAnimTo0);
     }
+}
+
+
+
+
+
+function tst(nb){
+    let array = new Array(nb);
+    for(let i = 0; i < nb; ++i){
+        array[i] = Math.floor(Math.random() * nb);
+    }
+
+    let euh = [
+        {age:2},
+        {age:50},
+        {age:14},
+        {age:90},
+        {age:1}
+    ]
+
+    let start = Date.now();
+    // console.log(triFusion(euh, (g, d) => g.age < d.age));
+    triFusion(array);
+    let fin = Date.now();
+    console.log(`TEMPS DEXEC + ${fin-start}MS`);
+    // console.log(triFusion(array));
 }
