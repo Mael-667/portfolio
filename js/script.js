d21e78698826e62d995505d6359df57c1bbcba0c
Mael-667 <flamentmael@gmail.com>
2025-09-14 19:21:15 +0200

    déplacement de toute la logique liquidGlass vers le framework de moi

--- js/script.js
+++ js/script.js
@@ -10,8 +10,8 @@ function delEvt(target, event, fun){
     target.removeEventListener(event, fun);
 }
 
-function get(element){
-    return document.querySelector(element);
+function get(element, target = document){
+    return target.querySelector(element);
 }
 
 function toggleAnim(target, cssClass){
@@ -76,83 +76,6 @@ function isTargetInElement(target, element){
 }
 
 
-
-//ajouter "data-hue:#hexcolor" a un élement pour que sa couleur imprègne les liquidglass ayant pour parent un élément a la class dynamic qui le survolent
-function dynamicColor(){
-
-    let backgrounds = document.querySelectorAll("[data-hue]")
-    let parents = document.querySelectorAll(".dynamic")
-    let elmnts = [];
-    for(parent of parents){ 
-        for(enfant of parent.querySelectorAll(".liquidGlass")){
-            elmnts.push(enfant);
-        }
-    } 
-
-
-    addEvt(document, "scroll", function(){
-        let selected = [];
-
-        for(let i = 0; i < backgrounds.length; ++i){
-            let color = backgrounds[i].getAttribute("data-hue");
-            for(let j = 0; j < elmnts.length; j++){
-                if(isTargetInElement(elmnts[j], backgrounds[i])){
-                    elmnts[j].classList.add("dynamic")
-                    elmnts[j].style.setProperty('--background', `${pSBC(0.01, color)}07`);
-                    elmnts[j].style.setProperty('--shadow', `inset 1px 1px 3px 1px ${pSBC(-0.2, color)}20, inset -1px -2px 3px 1px rgb(49 49 49 / 30%), 0px 0px 7px 1px rgb(0 0 0 / 46%)`);
-                    selected.push(j);
-                }
-            }
-        }
-        for(let i = 0; i < elmnts.length; ++i){
-            let euh = true;
-            for(let j = 0; j < selected.length; j++){
-                if(i == selected[j]){
-                    euh = false;
-                }
-            }
-            if(euh){
-                elmnts[i].classList.remove("dynamic")
-                elmnts[i].style.setProperty('--background', "");
-                elmnts[i].style.setProperty('--shadow', ``);
-            }
-        }
-    })
-}
-
-
-
-const pSBC=(p,c0,c1,l)=>{
-    let r,g,b,P,f,t,h,i=parseInt,m=Math.round,a=typeof(c1)=="string";
-    if(typeof(p)!="number"||p<-1||p>1||typeof(c0)!="string"||(c0[0]!='r'&&c0[0]!='#')||(c1&&!a))return null;
-    if(!this.pSBCr)this.pSBCr=(d)=>{
-        let n=d.length,x={};
-        if(n>9){
-            [r,g,b,a]=d=d.split(","),n=d.length;
-            if(n<3||n>4)return null;
-            x.r=i(r[3]=="a"?r.slice(5):r.slice(4)),x.g=i(g),x.b=i(b),x.a=a?parseFloat(a):-1
-        }else{
-            if(n==8||n==6||n<4)return null;
-            if(n<6)d="#"+d[1]+d[1]+d[2]+d[2]+d[3]+d[3]+(n>4?d[4]+d[4]:"");
-            d=i(d.slice(1),16);
-            if(n==9||n==5)x.r=d>>24&255,x.g=d>>16&255,x.b=d>>8&255,x.a=m((d&255)/0.255)/1000;
-            else x.r=d>>16,x.g=d>>8&255,x.b=d&255,x.a=-1
-        }return x};
-    h=c0.length>9,h=a?c1.length>9?true:c1=="c"?!h:false:h,f=this.pSBCr(c0),P=p<0,t=c1&&c1!="c"?this.pSBCr(c1):P?{r:0,g:0,b:0,a:-1}:{r:255,g:255,b:255,a:-1},p=P?p*-1:p,P=1-p;
-    if(!f||!t)return null;
-    if(l)r=m(P*f.r+p*t.r),g=m(P*f.g+p*t.g),b=m(P*f.b+p*t.b);
-    else r=m((P*f.r**2+p*t.r**2)**0.5),g=m((P*f.g**2+p*t.g**2)**0.5),b=m((P*f.b**2+p*t.b**2)**0.5);
-    a=f.a,t=t.a,f=a>=0||t>=0,a=f?a<0?t:t<0?a:a*P+t*p:0;
-    if(h)return"rgb"+(f?"a(":"(")+r+","+g+","+b+(f?","+m(a*1000)/1000:"")+")";
-    else return"#"+(4294967296+r*16777216+g*65536+b*256+(f?m(a*255):0)).toString(16).slice(1,f?undefined:-2)
-}
-
-
-
-
-
-
-
 let intro = document.querySelector("#intro");
 let divProjet = get("#projets");
 let projetsAnimSetuped = false;
@@ -160,11 +83,11 @@ document.addEventListener("scroll", function(e){
     let euh = isElementInViewport(intro); 
     if(euh){
         document.querySelectorAll("header .liquidGlass").forEach(e => 
-            e.classList.remove("glassDarkMode")
+            e.classList.add("glassLightMode")
         );
     } else {
         document.querySelectorAll("header .liquidGlass").forEach(e =>
-                e.classList.add("glassDarkMode")
+            e.classList.remove("glassLightMode")
         );
     }
 })
@@ -392,7 +315,7 @@ function insertProject(data) {
         readme = `${data.readme.substring(0, 150)}...`;
     }
     let toInsert= `
-                    <article class="liquidGlass liquidGlassLarge darkGlassMode projet">
+                    <article class="liquidGlassLarge projet">
 
                         <div class="minia" style="background-image: url('${data.img}');"> </div>
 
